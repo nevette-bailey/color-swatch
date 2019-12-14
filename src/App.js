483524af1d17306474755dd6a7c492c46103624e
Nevette Bailey <nevettebailey@gmail.com>
2019-12-14 14:53:12 -0500

    add: color swatch base

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
+        <div id='flex-container'>
+          <div className='colorSwatch'></div>
+          <div className='container'>
+            <button type='button' id='redder'>
+              Redder
+            </button>
+            <button type='button' id='bluer'>
+              Bluer
+            </button>
+            <button type='button' id='greener'>
+              Greener
+            </button>
+          </div>
+        </div>
+
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
