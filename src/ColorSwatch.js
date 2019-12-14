1e6ef6006778bf5f45103cb4e34bd986e76c2a84
Nevette Bailey <nevettebailey@gmail.com>
2019-12-14 16:13:15 -0500

    add: redder

--- src/ColorSwatch.js
+++ src/ColorSwatch.js
@@ -3,24 +3,41 @@ import React from 'react';
 class ColorSwatch extends React.Component {
   constructor() {
     super();
-    this.state = { red: 0, blue: 0, green: 255 };
-    this.style = {
-      padding: '100px',
-      color: 'blue',
-      backgroundColor: `rgb(${this.state.red}, ${this.state.green}, ${this.state.blue})`,
+    this.state = {
+      red: 0,
+      green: 255,
+      blue: 0,
+      style: {
+        padding: '100px',
+        backgroundColor: `rgb(0, 255, 0)`,
+      },
     };
     this.handleClickRedder = this.handleClickRedder.bind(this);
   }
 
   handleClickRedder(event) {
     event.preventDefault();
+    let red = Math.floor(this.state.red * 0.9 + 0.1 * 255);
+    let green = Math.floor(this.state.green * 0.9);
+    let blue = Math.floor(this.state.blue * 0.9);
+
+    console.log('RED CLICKED', red, this.state);
+    this.setState({
+      red: red,
+      green: green,
+      blue: blue,
+      style: {
+        padding: '100px',
+        backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+      },
+    });
   }
 
   render() {
     return (
       <div>
         <div id='flex-container'>
-          <div className='colorSwatch' style={this.style}></div>
+          <div className='colorSwatch' style={this.state.style}></div>
           <div className='container'>
             <button type='button' id='redder' onClick={this.handleClickRedder}>
               Redder
